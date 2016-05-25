054a099f85bb1720f36aa7111a420ba5e57c0682
flowstyleliving <mys628@gmail.com>
2016-05-25 14:19:34 -0700

    regEx resource

---
+++ regEx/regEx.js
@@ -0,0 +1,36 @@
+/*
+Identifiers:
+
+\d = any number
+\D = anything but a number
+\s = space
+\S = anything but a space
+\w = letters
+\W = anything but a letter
+. = any character except for new line
+\b = word word2 word3
+\. =
+
+Modifiers:
+
+\d{1,5} = 1-5
++ = matches 1 or more
+? = matches 0 or 1
+* = 0 or more
+$ match end of a string
+^ = match start of a string
+| = or
+[] = variants
+{x} = thi amount of preceding code
+
+White space characters:
+\n = new line
+\s = sspace
+\tab = table
+\e = escape
+\r = carriage return
+\f = form feed
+
+escape required
+
+ */
