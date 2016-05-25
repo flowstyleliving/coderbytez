1052a58a37a9b7a8cd7b63db0644d9e8e1715fe6
flowstyleliving <mys628@gmail.com>
2016-05-25 14:57:15 -0700

    updated regEx

--- regEx/regEx.js
+++ regEx/regEx.js
@@ -5,15 +5,16 @@ Identifiers:
 \D = anything but a number
 \s = space
 \S = anything but a space
-\w = letters
-\W = anything but a letter
+\w = any character
+\W = anything but a character
 . = any character except for new line
-\b = word word2 word3
-\. =
+\b = word1 word2 word3 //matches space after any word
+\. = .
+
 
 Modifiers:
 
-\d{1,5} = 1-5
+\d{1,5} = digit 1-5 in length
 + = matches 1 or more
 ? = matches 0 or 1
 * = 0 or more
@@ -21,16 +22,28 @@ $ match end of a string
 ^ = match start of a string
 | = or
 [] = variants
-{x} = thi amount of preceding code
+{x} = this amount of preceding code
 
 White space characters:
 \n = new line
-\s = sspace
-\tab = table
-\e = escape
+\s = space
+\tab = tab
+\e = escape white space
 \r = carriage return
 \f = form feed
 
-escape required
+escape required:
+$ . () [] \ ^ + ? |
 
  */
+//
+// 123 Main St.
+//
+// \d{1,5}\s\w+\s\w+\.
+//
+// \$\d*\.\d{2}
+// $100
+//
+// [a-z] all lowercase letters
+// [A-Z] capital
+// [A-Za-z0-9]
