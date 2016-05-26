12746e60039505757bfcd533857315b0a0ecc4f8
flowstyleliving <mys628@gmail.com>
2016-05-26 14:25:09 -0700

    optimized

--- freeCodeCamp/ChunkeyMonkey.js
+++ freeCodeCamp/ChunkeyMonkey.js
@@ -2,11 +2,9 @@ function chunkArrayInGroups(arr, size) {
   var newArr = [];
   var i = 0;
   while(arr.length > 0) {
-    var x = arr.splice(i, size);
-      console.log('x', x);
-       newArr.push(x);
-       i = 0;
-       console.log(newArr);
+    newArr.push(arr.splice(i, size));
+      // console.log('x', x);
+      //  console.log(newArr);
   }
 
   return newArr;
