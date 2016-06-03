c6955015361c79108f0cf6709d4c50d8ac81dcbc
flowstyleliving <mys628@gmail.com>
2016-06-03 13:19:11 -0700

    BoolWho

---
+++ freeCodeCamp/BoolWho.js
@@ -0,0 +1,15 @@
+/*
+  Learnings:
+  -Boolean(is it true? false? :)
+ */
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  if(bool === true) {
+    return true;
+  } else if(bool === false){
+    return true;
+  } else return false;
+  //return bool;
+}
+
+booWho(null);
